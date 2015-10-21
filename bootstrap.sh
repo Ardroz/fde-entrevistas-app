d66b18c2fd6c471a765d45704e2f22bba355ac68
ardroz <darr.0725@gmail.com>
2015-10-21 12:22:10 -0500

    :rocket: Base polymer app + Vagrant

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+sudo apt-get install --yes nodejs git build-essential
+sudo npm install -g yo bower generator-polymer node-gyp
